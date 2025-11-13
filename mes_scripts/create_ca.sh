2d88db4619fb6aa7d77cf488e1dbbb31cdf1fd72
mahamadou <mahamadou.gory.sio@gmail.com>
2025-11-13 16:31:28 +0100

    FEAT: generate and auto-signed CA certificat in create_ca.sh file

--- mes_scripts/create_ca.sh
+++ mes_scripts/create_ca.sh
@@ -1,1 +1,36 @@
-#création du ca (a mettre les certifvats généré dans le /pki/ca)
+#!/bin/bash
+
+echo "---création de l'autorité de certification---" 
+
+ source "$(dirname "$0")/../variables.conf"
+
+mkdir -p $CA_DIR
+mkdir -p $SERVER_DIR
+mkdir -p $CLIENT_DIR
+
+echo "on génere la clé privée de la CA"
+
+openssl genpkey -algorithm RSA -pkopt rsa_keygen_bits:4096 \
+    -aes256 -passout pass:$CA_PASS \
+    -out $CA_DIR/ca.key
+
+
+#[ $? -ne 0 ] → signifie “si le code de retour n’est pas égal à 0”
+if [ $? -ne 0 ]; then
+    echo "ERREUR: Échec de la génération de la clé CA"
+    exit 1
+fi
+
+echo "Génération du certificat auto-signé"
+
+openssl req -x509 -new -nodes \
+    -key $CA_DIR/ca.key \
+    -passin pass:$CA_PASS \
+    -sha256 -days 365 \
+    -subj "/C=FR/ST=France/L=Paris/O=ESGI/CN=esgi CA" \
+    -out $CA_DIR/ca.crt
+
+if [ $? -ne 0 ]; then
+    echo "ERREUR: Échec de la génération de la clé CA"
+    exit 1
+fi
